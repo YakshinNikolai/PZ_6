7f2be58093aa8edb13774ba96ff193a953a3d60d
YakshinNikolai <nikolai1115@bk.ru>
2024-10-15 10:22:32 +0300

    Add files via upload

---
+++ Shaxov_Yakshin_2/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace std;
+class Program
+{
+    static void Main()
+    {
+        string str;
+        str = "ФИО: {{ {0} }}, Возраст: {{ {1} }}, Дата рождения {{ {2} }}, Место рождения: {{ {3} }}";
+        str = String.Format(str, "Шахов С.Ю.", 18, "06.01.2006", "г.Курск");
+        string str1;
+        str1 = "ФИО: {{ {0} }}, Возраст: {{ {1} }}, Дата рождения {{ {2} }}, Место рождения: {{ {3} }}";
+        str1 = String.Format(str1, "Якшин Н.Н.", 17, "31.05.2007", "г.Курск");
+        Console.WriteLine(str);
+        Console.WriteLine(str1);
+    }
+}
